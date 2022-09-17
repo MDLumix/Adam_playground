46d477bc0c8f5099fceadf1d2f69c674fe961a58
Marek <marek.dubiec@gmail.com>
2022-09-17 22:14:10 +0200

    Added circles.py

---
+++ circles.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+def circles(number, radius,radius2):
+    angle=360/number
+    n=0
+    while n<number:
+        penup()
+        forward(radius)
+        pendown()
+        circle(radius2)
+        penup()
+        back(radius)
+        left(angle)
+        n=n+1
+
+circles(8,10,75)
+input()
